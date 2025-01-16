2302394f891a4444b062557b95d8f324eae2a243
Shuviu <Shuviu.Dev@gmail.com>
2025-01-16 22:29:46 +0000

    Created basic terminal component

---
+++ src/components/Terminal.jsx
@@ -0,0 +1,23 @@
+import './Terminal.css'
+
+function Terminal() {
+    return (
+        <div className='terminal'>
+            <div className="terminal-header">
+                <div className="terminal-buttonrow">
+                    <button>File</button>
+                    <button>Edit</button>
+                    <button>View</button>
+                </div>
+                <div>
+                    <span>shuviu@portfolio-os</span>
+                </div>
+            </div>
+            <div className='terminal-body'>
+                owa
+            </div>
+        </div>
+    )
+}
+
+export default Terminal
